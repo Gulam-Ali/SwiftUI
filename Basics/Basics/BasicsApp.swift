f1dd3ca6223f396f1ce3510cbfa2d34440c24fed
Gulam Ali <gulam2275@gmail.com>
2022-06-07 15:58:48 +0530

    First proj added

---
+++ Basics/Basics/BasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicsApp.swift
+//  Basics
+//
+//  Created by apple on 07/06/22.
+//
+
+import SwiftUI
+
+@main
+struct BasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
